4899dfda54791aea61daf8620a6bdd410070d739
Kreeblah <kreeblah@gmail.com>
2020-07-07 13:35:56 -0700

    Added license header.

--- TESmartSwitchAPI/ViewController.m
+++ TESmartSwitchAPI/ViewController.m
@@ -1,9 +1,27 @@
+/*
+     This file is part of TESmart Switch API at:
+     <https://github.com/Kreeblah/TESmartSwitchAPI-macOS>.
+
+TESmart Switch API is free software: you can redistribute it and/or modify
+it under the terms of the Affero GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TESmartSwitchAPI-macOS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Affero GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TESmart Switch API.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //
 //  ViewController.m
 //  TESmartSwitchAPI
 //
 //  Created by Chris Gelatt on 7/4/20.
-//  Copyright © 2020 Kreeblah. All rights reserved.
+//  Copyright © 2020 Chris Gelatt. All rights reserved.
 //
 
 #import "ViewController.h"
