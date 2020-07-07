0fcb6553995587d564ca80d8a364bd67c5803ea3
Kreeblah <kreeblah@gmail.com>
2020-07-07 13:24:36 -0700

    Removed extraneous logging.

--- TESmartSwitchAPI/IPAddressFormatter.m
+++ TESmartSwitchAPI/IPAddressFormatter.m
@@ -25,11 +25,8 @@
 
 - (BOOL)isPartialStringValid:(NSString *)partialString newEditingString:(NSString * _Nullable __autoreleasing *)newString errorDescription:(NSString * _Nullable __autoreleasing *)error {
     if([partialString length] == 0) {
-        NSLog(@"Found zero-length IP address string.");
         return YES;
     }
-    
-    NSLog(@"Searching matches for IP address string: %@", partialString);
     
     NSRange searchRange = NSMakeRange(0, [partialString length]);
     NSString* ipv4Pattern = @"^(25[0-5]\\.|2[0-4][0-9]\\.|[01]?[0-9][0-9]?\\.){0,3}?((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))?$";
@@ -37,8 +34,6 @@
     
     NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:ipv4Pattern options:0 error:&searchError];
     unsigned long regexMatches = [regex numberOfMatchesInString:partialString options:0 range:searchRange];
-    
-    NSLog(@"Found %ld matches for IP address string: %@", regexMatches, partialString);
     
     if(regexMatches == 0) {
         return NO;
